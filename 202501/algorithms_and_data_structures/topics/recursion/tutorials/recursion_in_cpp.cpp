baab09fc2677dbad01103c5ee71241a2286f88d9
Nicole Sanchez <nicoleabsanchez@gmail.com>
2025-05-18 18:09:43 -0500

    a

--- 202501/algorithms_and_data_structures/topics/recursion/tutorials/recursion_in_cpp.cpp
+++ 202501/algorithms_and_data_structures/topics/recursion/tutorials/recursion_in_cpp.cpp
@@ -1,7 +1,17 @@
+// This is a simple C++ program that demonstrates recursion by calculating the factorial of a number. 
+// Credits: BroCode on YT
 #include <iostream>
 using namespace std;
 
+int factorial(int n){
+    if (n>1){
+        return n*factorial(n-1);
+    }
+    else { return 1;}
+}
+
 int main(){
     cout << "Hello, World!" << endl;
+    cout << factorial(5) << endl;
     return 0;
 }
