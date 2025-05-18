b6cea39129cad70cc972bc0e0cc4f1d77b329002
Nicole Sanchez <nicoleabsanchez@gmail.com>
2025-05-17 23:14:18 -0500

    a

--- 202501/algorithms_and_data_structures/recursion/walk_and_factorial.cpp
+++ 202501/algorithms_and_data_structures/recursion/walk_and_factorial.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 using namespace std;
 
+// void walk(int steps){
+//     if(steps>0){
+//         cout << "You take a step" <<endl;
+//         walk(steps-1);
+//     }
+// }
+
 int main(){
-    walk(100);
+    // walk(100);
     return 0;
 }
 
-void walk(int steps){
-    if(steps>0){
-        cout << "You take a step" <<endl;
-        walk(steps-1);
-    }
-}
+
