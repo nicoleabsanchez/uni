330bfcd2f4843aff7c44593cac47e50aec8b6c54
Nicole Sanchez <nicoleabsanchez@gmail.com>
2025-05-18 17:10:17 -0500

    a

--- 202501/algorithms_and_data_structures/recursion/walk_and_factorial.cpp
+++ 202501/algorithms_and_data_structures/recursion/walk_and_factorial.cpp
@@ -8,7 +8,17 @@ using namespace std;
 //     }
 // }
 
+int factorial(int num){
+    if (num>1){
+        return num*factorial(num-1);
+    }
+    else {
+        return 1;
+    }
+}
+
 int main(){
+    cout<< factorial(10)<<endl;
     // walk(100);
     return 0;
 }
